1a3f0d85ac5295832050e0d97c28c12287ec49fe
Théo Torregrossa <theotor83@gmail.com>
2025-02-11 21:46:56 +0100

    1
    1

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog_project',
+    'blog.apps.BlogConfig',  
 ]
 
 MIDDLEWARE = [

7de91d3ebbc006df5df9da4aafc9e252963fb4ec
Théo Torregrossa <theotor83@gmail.com>
2025-02-13 15:32:27 +0100

    Added login logic and pages
    TODO: Add create new post logic

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:postid>', views.postdetails, name='post-details'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import BlogPost
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
@@ -10,3 +12,28 @@ def index(request):
 def postdetails(request, postid):
     post = get_object_or_404(BlogPost, pk=postid)
     return render(request,"detail.html",{"post": post})
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
